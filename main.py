a8d2e18d6cf9a85d3522e79f7b459262e86f8722
Django-Argus <66014216+Django-Argus@users.noreply.github.com>
2022-11-08 22:23:00 +0100

    Update

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
+import json
 import sys
-from json import *
 
 running = True
+
+FILE = "data.json"
 
 def loop():
     """boucle principale du programme"""
@@ -14,6 +16,8 @@ def loop():
             add()
         elif command == 2:
             get()
+        elif command == 3:
+            delete()
 
 
 def exit():
@@ -24,19 +28,94 @@ def add():
     """fonction pour ajouter un nouveau contact"""
     loop = True
     while loop:
-        name = inputStr("Nom (0 pour terminer)")
-        if name == 0:
+        name = inputStr("Nom (0 pour terminer): ")
+        if name == '0':
             loop = False
             continue
 
-        num = inputPhoneNumber("Numéro de téléphone: ")
-        #if num is None retry
+        if exist(name):
+            print("Erreur: ce nom exist deja !")
+            continue
+
+        num = inputPhoneNumber("Numero de telephone: ")
+        if num is None:
+            print("Erreur: le numero de telephone n'est pas valide !")
+            continue
+
+        addContact(name, num)
 
     pass
 
 def get():
-    """fonction pour récupérer  un contact"""
+    """fonction pour recuperer  un contact"""
+    loop = True
+    while loop:
+        name = inputStr("Nom (0 pour terminer): ")
+        if name == '0':
+            loop = False
+            continue
+
+        result = getContact(name)
+
+        if result == None:
+            print("Erreur: le nom est introuvable")
+            continue
+
+        print("Le numero de telephone de " + name + " est: " + result)
+        return
+
     pass
+
+def delete():
+    loop = True
+    while loop:
+        name = inputStr("Nom (0 pour terminer): ")
+        if name == '0':
+            loop = False
+            continue
+
+        if not exist(name):
+            print("Erreur: " + name + "n'existe pas !")
+            continue
+
+        if not consent("Etes-vous sur de vouloir supprimer " + name + " ?", True):
+            print("[End]")
+            continue
+
+        delContact(name)
+        print(name + " a bien ete supprime")
+
+
+
+def exist(name):
+   return index(name) is not None
+
+def index(name):
+    content = getFile()
+    for i in content:
+        if i['name'] == name:
+            return i
+
+def addContact(name, number):
+    content = getFile()
+
+    content.append({"name": name, "number": number})
+    writeFile(content)
+
+def getContact(name):
+    i = index(name)
+    if i is not None:
+        return i['number']
+
+def delContact(name):
+    i = index(name)
+    if i is None:
+        return
+
+    content = getFile()
+
+    content.remove(i)
+    writeFile(content)
 
 #                                   #-----****-----#                                   #
 
@@ -59,7 +138,9 @@ def inputPhoneNumber(*title):
     inp = inputInt(title)
     if inp is None:
         return
+
     inp = str(inp)
+    inp = '0' + inp
 
     if len(inp) != 10:
         return
@@ -77,4 +158,28 @@ def inputValue(title):
             title = ""
             break
     return input(title)
+
+def getFile():
+    """recupere le contenu du fichier"""
+    with open(FILE, "r") as f:
+        return json.load(f)
+
+def writeFile(data):
+    """ecrit le contenu du fichier"""
+    with open(FILE, "w") as f:
+        json.dump(data, f)
+
+def consent(title, yes):
+    text = "[oui;NON]"
+    if yes:
+      text = "[OUI;non]"
+
+    s = inputStr(title + " " + text)
+
+    if len(s) == 0:
+        return yes
+
+    return s.upper() == 'OUI'
+
+
 
